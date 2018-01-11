0d7762d5fbbe46d99f80dc7b1bfdbd17b2b50841
cmarr31 <chris@sitefuelusa.com>
2018-01-10 22:45:31 -0600

    Multiplication table assignment.

---
+++ week_1/multiplication_table.py
@@ -0,0 +1,10 @@
+def multiplication_table():
+    x = 1
+    while (x <= 12):
+        i = 1
+        while (i <= 12):
+            print i * x
+            i +=1
+        x +=1
+
+multiplication_table()
